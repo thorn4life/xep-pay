adbf880ed12c5ee7ab1f24b7983373239cebeefc
thorn4life <78955975+thorn4life@users.noreply.github.com>
2021-02-12 10:42:26 +0100

    Add files via upload

---
+++ electrapay_woocommerce_plugin.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: ElectraPay for WooCommerce
+Plugin URI: https://github.com/rfingberg/ThoRn-XEP
+Description: Extends WooCommerce with ElectraPay gateway.
+Version: 1.0
+Author: ThoRn, electraprotocol
+License: GPL v3
+License URI: https://www.gnu.org/licenses/gpl-3.0.en.html
+ */
